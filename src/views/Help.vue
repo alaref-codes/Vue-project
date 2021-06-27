bff039b9df76614df23669f3ac229b5fabc3f1fb
alaref-codes <alarefabushaala@gmail.com>
2021-06-27 07:31:15 +0200

    Added the help page

---
+++ src/views/Help.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="help-me" >
+        <h1>Help</h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit, hic!</p>
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: 'Help'
+}
+</script>
