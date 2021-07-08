f650145151f0ababdbf98f6962b448a3e1709dcf
alaref-codes <alarefabushaala@gmail.com>
2021-07-08 19:32:55 +0200

    added github link

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <h2>Welcome to Your Vue.js app</h2>
+    <a style="text-decoration: none; font-size: 1.2rem; color: black; font-weight: bold; border: 1px solid black;  " href="https://github.com/alaref-codes/Vue-project/">Github repository</a>
     <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
   </div>
 </template>
